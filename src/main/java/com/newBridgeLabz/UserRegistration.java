6c9c66baeda191c4d4ce67cd76450ae197efdce6
RakeshSR10 <rakeshsrking@gmail.com>
2021-04-18 12:31:54 +0530

    [Added] Welcome to User Registration

---
+++ src/main/java/com/newBridgeLabz/UserRegistration.java
@@ -0,0 +1,8 @@
+package com.newBridgeLabz;
+
+public class UserRegistration
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration Program");
+    }
+}
