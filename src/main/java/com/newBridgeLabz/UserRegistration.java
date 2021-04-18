799f70e7dcad00627fb4a27373f095f1cfcefd42
RakeshSR10 <rakeshsrking@gmail.com>
2021-04-18 14:01:45 +0530

    [Added] First_Name with minimum character

--- src/main/java/com/newBridgeLabz/UserRegistration.java
+++ src/main/java/com/newBridgeLabz/UserRegistration.java
@@ -1,8 +1,27 @@
 package com.newBridgeLabz;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class UserRegistration
 {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Program");
+        Scanner scanner = new Scanner(System.in);
+
+        //UC1 User need to enter valid First Name
+        String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+        System.out.println("Enter First Name:");
+        String First_Name = scanner.next();
+
+        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+        Matcher matcher = pattern.matcher(First_Name);
+
+        boolean result = matcher.matches();
+        if(result == true)
+            System.out.println("Valid");
+        else
+            System.out.println("Not valid");
     }
 }
